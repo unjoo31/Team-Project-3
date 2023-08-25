739b6d363e2f61511fa03fad8f99009db6d52577
unyak <0106un@naver.com>
2023-08-25 11:52:24 +0900

    댓글 삭제 완료

--- src/main/java/shop/mtcoding/blogv2/reply/ReplyController.java
+++ src/main/java/shop/mtcoding/blogv2/reply/ReplyController.java
@@ -1,8 +1,16 @@
 package shop.mtcoding.blogv2.reply;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReplyController {
+    
+    @PostMapping("/reply/{id}/detele")
+    public String detele(Integer id){
+        System.out.println("댓글 삭제 완료");
+        return "redirect:/board/"+id;
+    }
 
 }
