0b594830c270eb98136752252c60f904c6d896e1
kevinyun31 <kevinyun31@gmail.com>
2023-08-25 11:18:05 +0900

    윤지환 회원정보 보기

--- src/main/java/shop/mtcoding/blogv2/user/UserController.java
+++ src/main/java/shop/mtcoding/blogv2/user/UserController.java
@@ -1,8 +1,16 @@
 package shop.mtcoding.blogv2.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+ // 회원정보 화면 호출
+    @GetMapping("/user/updateForm")
+    public String updateForm() {
+      
+        return "user/updateForm";
+    }
 
 }
