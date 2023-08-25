b578b0c66385a114b9420786f18d886541770441
unyak <0106un@naver.com>
2023-08-25 11:17:43 +0900

    로그인 화면, 기능 구현 완료

--- src/main/java/shop/mtcoding/blogv2/user/UserController.java
+++ src/main/java/shop/mtcoding/blogv2/user/UserController.java
@@ -1,8 +1,23 @@
 package shop.mtcoding.blogv2.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        System.out.println("로그인 페이지 완료");
+        return "/loginForm";
+    } 
+
+    @PostMapping("/login")
+    public String login(){
+        System.out.println("로그인 구현 완료");
+        return "redirect:/";
+    }
 
 }
